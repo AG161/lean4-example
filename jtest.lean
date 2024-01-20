74484066f308ffd2f5c804fe1f653f28c2425c59
AG161 <81839540+AG161@users.noreply.github.com>
2024-01-19 19:07:18 -0500

    Create jtest.lean

---
+++ jtest.lean
@@ -0,0 +1,12 @@
+theorem dojo2_uncombined (p : Prop) : p ∨ p → p := by
+  intro h
+  cases h
+  assumption
+  assumption
+
+theorem jtest (n : Nat) : n = n := by
+  sorry
+
+theorem dojo2_combined (p : Prop) : p ∨ p → p := by
+  intro h
+  cases h <;> assumption
