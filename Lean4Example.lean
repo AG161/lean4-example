bd4ef71f162b0b57d774b70e3cd1aa09464041cc
AG161 <81839540+AG161@users.noreply.github.com>
2024-01-19 23:28:39 -0500

    Update Lean4Example.lean

--- Lean4Example.lean
+++ Lean4Example.lean
@@ -4,10 +4,8 @@ theorem dojo2_uncombined (p : Prop) : p ∨ p → p := by
   assumption
   assumption
 
-/-
 theorem jtest (n : Nat) : n = n := by
   sorry
--/
 
 theorem dojo2_combined (p : Prop) : p ∨ p → p := by
   intro h
